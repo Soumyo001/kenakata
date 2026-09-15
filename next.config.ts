4772715cb709573121680fe442060247fd9a812d
Soumyo001 <www.soumyo@gmail.com>
2026-09-15 10:30:25 +0600

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+      remotePatterns: [
+          { protocol: "https", hostname: "i.imgur.com" },
+          { protocol: "https", hostname: "placeimg.com" },
+          { protocol: "https", hostname: "**.escuelajs.co" },
+      ],
+  },
 };
 
 export default nextConfig;
